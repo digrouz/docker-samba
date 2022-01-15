bccc42f60c63050e9b4d466ff74df6bee6e0e93b
Nicolas Di Gregorio <nicolas.digregorio@gmail.com>
2022-01-15 18:23:17 +0100

    Update docker-entrypoint.sh

--- root/usr/local/bin/docker-entrypoint.sh
+++ root/usr/local/bin/docker-entrypoint.sh
@@ -14,7 +14,7 @@ if [ "$1" == 'samba' ]; then
   DockLog "Fixing permissions on /var/log/samba"
   chown -R ${MYUSER}:${MYUSER} /var/log/samba
   DockLog "Starting app: ${1}"
-  exec smbd -FS --no-process-group
+  exec smbd -F --no-process-group
 else
   DockLog "Starting app: ${@}"
   exec "$@"
