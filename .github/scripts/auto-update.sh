94cc2d8f66be50c4e3b4197e164ca22bc9eea23f
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2025-02-27 20:30:12 +0000

    fix autoupdate

--- .github/scripts/auto-update.sh
+++ .github/scripts/auto-update.sh
@@ -4,7 +4,7 @@ PACKAGE_NAME=samba;
 PACKAGE_ARCH=x86_64; 
 ALPINE_VERSION=$(grep "FROM alpine:" Dockerfile_alpine | awk '{print $2}'| sed  -E "s|alpine:||" | awk -F '.' '{print $1"."$2}')
 
-NEW_VERSION=$(curl -SsL https://pkgs.alpinelinux.org/package/v${ALPINE_VERSION}/main/${PACKAGE_ARCH}/${PACKAGE_NAME} | grep -i -A 2 version | tail -1 | awk '{print $1}')
+NEW_VERSION=$(curl -SsL https://pkgs.alpinelinux.org/package/v${ALPINE_VERSION}/main/${PACKAGE_ARCH}/${PACKAGE_NAME} | grep -i -A 3 version | tail -1 | awk '{print $1}' | sed  -e 's|<strong>||g' -e 's|</strong>||g')
 sed -i -e "s|ARG ${PACKAGE_NAME^^}_VERSION=.*|ARG ${PACKAGE_NAME^^}_VERSION=${NEW_VERSION}|" Dockerfile_alpine
 
 if output=$(git status --porcelain) && [ -z "$output" ]; then
